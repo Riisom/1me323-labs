5c913af940b9b8b529d229eb61091837e8969b4c
Alexander Riisom <arr224qg@student.lnu.se>
2026-02-03 18:27:42 +0100

    Lab 1-a: initial commit

---
+++ 1a/mains.js
@@ -0,0 +1,13 @@
+import divide, { add, subtract, multiply, PI, SECRET_SAUCE } from './math.js';
+
+console.log(SECRET_SAUCE);
+console.log(`PI är ungefär ${PI}`);
+
+console.log(add(5, 3));
+console.log(subtract(10, 4));
+console.log(multiply(3, 7));
+console.log(divide(20, 4));
+
+
+console.log(SECRET_SAUCE);
+console.log(`PI är ungefär ${PI}`);
