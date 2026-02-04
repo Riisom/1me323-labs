20523a7da90c51217278741a2676f0b7d31609d9
Alexander Riisom <arr224qg@student.lnu.se>
2026-02-04 13:29:08 +0100

    index.html

--- 1a/mains.js
+++ 1a/mains.js
@@ -8,6 +8,9 @@ console.log(subtract(10, 4));
 console.log(multiply(3, 7));
 console.log(divide(20, 4));
 
+// Testlogg för diff-vyn
+console.log("Hej från main.js!");
+import { rectangleArea, rectanglePerimeter } from './rectangles.js';
 
-console.log(SECRET_SAUCE);
-console.log(`PI är ungefär ${PI}`);
+console.log(`Area of 5x10 rectangle: ${rectangleArea(5, 10)}`);
+console.log(`Perimeter of 5x10 rectangle: ${rectanglePerimeter(5, 10)}`);
